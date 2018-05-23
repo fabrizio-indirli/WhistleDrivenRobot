ac93e952205ac114f41097f789b3aa78399fd7af
Daniele <daniele.borgo@mail.polimi.it>
2018-05-23 18:50:39 +0200

    Added the possibility to perform different compilation, optimized without parameters and not optimized.

--- receiver/receiver.ino
+++ receiver/receiver.ino
@@ -1,31 +1,45 @@
+#include "src/commands/Executer.h"
+/**
+ * If this define is enabled, the compiler will create a code that executes
+ * the received commands without expecting parameters. This choice increase
+ * the performarces but forbid any parameters sending in the Bluetooth channel.
+ */
+#define OPTIMIZATION 1
+#ifndef OPTIMIZATION
 #include "src/communication/BTSerialCommunication.h"
 #include "src/communication/USBSerialCommunication.h"
+#else
 #include "src/optimized-communication/optimized-communication.h"
-#include "src/hashmap/IntegerHashMap.h"
-#include "src/commands/Executer.h"
+#endif
 
-//SerialCommunication* communication;
-OptimizedCommunication opt_c;
-Executer* executer;
+#ifndef OPTIMIZATION
+SerialCommunication* communication;
 String* string;
+#else
+OptimizedCommunication optimized_communication;
+#endif
+Executer* executer;
 
 void setup() {
-    //communication = new BTSerialCommunication();
-    //communication = new USBSerialCommunication();
+#ifndef OPTIMIZATION
+    communication = new BTSerialCommunication();
+#endif
     executer = new Executer();
 }
 
 void loop() {
-    /*communication->updateBuffer();
+#ifndef OPTIMIZATION
+    communication->updateBuffer();
     if(communication->isTheLineAllRead()){
   	    string = communication->getReadLine();
   	    Serial.println(*string);
   	    executer->execute(string);
-    }*/
-    
-    if(opt_c.doYouHaveCharacters()){
-    	char c = opt_c.read();
-    	if(isALowerCaseLiteralChar(c))
-    		executer->optimizedExecute(c);
     }
+#else
+    if(optimized_communication.doYouHaveCharacters()){
+        char c = optimized_communication.read();
+        if(isALowerCaseLiteralChar(c))
+            executer->optimizedExecute(c);
+    }
+#endif
 }
